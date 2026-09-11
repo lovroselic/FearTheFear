41db3a0cfa8c6d89457e42e816b3050ae3ca9132
Lovro Selic <lovro.selic@gmail.com>
2026-09-11 19:33:43 +0200

    pics p1

--- Games/FearTheFear/FearTheFear.js
+++ Games/FearTheFear/FearTheFear.js
@@ -160,7 +160,7 @@ const INI = {
 /////////////////////////////////////////////
 
 const PRG = {
-    VERSION: "0.1.1",
+    VERSION: "0.1.2",
     NAME: "Fear The Fear",
     YEAR: "2026",
     SG: "FTF",
